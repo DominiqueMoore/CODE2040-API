6f20dd813831c3fe90ac69ef51f74c6cd0bf3df7
Dominique <dominique_moore@brown.edu>
2016-08-23 12:58:42 -0400

    Update Registration.java
    Implemented encapsulation, renamed constants

--- Registration.java
+++ Registration.java
@@ -1,46 +1,51 @@
+import java.io.IOException;
+
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 
-public class registration {
+public class Registration {
 	
-	String tokenValue = new String("d4198c3d90103fdfeb3b4bf85e52620f");
-	HttpClient client = HttpClientBuilder.create().build();
+	private final String TOKEN_VALUE = "d4198c3d90103fdfeb3b4bf85e52620f";
+	private final HttpClient CLIENT = HttpClientBuilder.create().build();
 	
-	public registration() {
-		this.makeRegistrationRequest();
+	private void handleAPIRequest() throws ClientProtocolException, IOException {
+		makeRegistrationRequest();
 	}
 	
-	public void makeRegistrationRequest() {
-		String github = new String("https://github.com/DominiqueMoore/CODE2040-API");
-		String registrationEndpoint = new String("http://challenge.code2040.org/api/register");
-		
-		HttpClient client = HttpClientBuilder.create().build(); //create Client
-		
-		try {
-			HttpPost request = new HttpPost(registrationEndpoint); //POST request
-			request.setEntity((new StringEntity("{\"token\":\"" + tokenValue + "\",\"github\":\"" + github + "\"}", 
-					ContentType.create("application/json"))));
-			HttpResponse response = client.execute(request); //POST response
+	
+	//learned difference between try-catch and throws 
+	private void makeRegistrationRequest() throws ClientProtocolException, IOException {
+		final String GITHUB = "https://github.com/DominiqueMoore/CODE2040-API";
+		final String REGISTRATION_ENDPOINT = "http://challenge.code2040.org/api/register";
+	
+		HttpPost postRequest = new HttpPost(REGISTRATION_ENDPOINT); //POST request
+		postRequest.setEntity((new StringEntity("{\"token\":\"" + TOKEN_VALUE + "\",\"github\":\"" + GITHUB + "\"}", 
+				ContentType.create("application/json"))));
+		HttpResponse postResponse = CLIENT.execute(postRequest); //POST response
 			
-			System.out.println("\nSending 'POST' request to URL : " + registrationEndpoint);
-			System.out.println("Post parameters : " + request.getEntity());
-			System.out.println("Response Code : " + response.getStatusLine().toString());
-		} catch (Exception e) {
-			
-		} finally {
-			
-		}
+		System.out.println("\nSending 'POST' request to URL : " + REGISTRATION_ENDPOINT);
+		System.out.println("Post parameters : " + postRequest.getEntity());
+		System.out.println("Response Code : " + postResponse.getStatusLine().toString());
+	
 		
 	}
 	
 	public static void main(String[] args) throws Exception {
-		registration obj = new registration();
-		stringReversal str = new stringReversal();
-		needleInHaystack nis = new needleInHaystack();
+		Registration obj = new Registration();
+		obj.handleAPIRequest();
+		StringReversal str = new StringReversal();
+		str.handleAPIRequest();	
+		NeedleInHaystack nis = new NeedleInHaystack();
+		nis.handleAPIRequest();
+		Prefix prefix = new Prefix();
+		prefix.handleAPIRequest();
+		TheDatingGame tdg = new TheDatingGame();
+		tdg.handleAPIRequest();
 	}
 
 }

2a225cd3445014ee7311ef3e49800e52a738819d
Dominique <dominique_moore@brown.edu>
2016-08-26 12:44:30 -0400

    Update Prefix.java
    updated final variables, private methods, JavaDocs

--- Prefix.java
+++ Prefix.java
@@ -1,10 +1,11 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
@@ -12,31 +13,29 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-
+/**
+ * With this exercise, I learned how to use Iterators, and that you cannot
+ * remove all substrings of a string from an ArrayList by use the .remove() method.
+ * This was also my first time removing Substrings from a String.
+ */
 public class Prefix {
-	byte[] bytes = null;
-	String prefixID = null;
-	String[] lastList = null;
-	String[] searchList = null;
-	String strings = null;
-	byte[][] finalBytes = null;
+	
+	private final String TOKEN_VALUE = "d4198c3d90103fdfeb3b4bf85e52620f";
+	private final HttpClient CLIENT = HttpClientBuilder.create().build();
+	String strings;
+	String prefixID;
+	JSONArray jArray;
+	String[] searchList;
 	int count = 0;
-	JSONArray jArray = null;
-	String apiResponse = null;
-	ArrayList<String> myList = null;
-	JSONArray lastJSONArray = null;
-
+	ArrayList<String> myList;
+	JSONArray lastJSONArray;
 	
-	HttpClient client = HttpClientBuilder.create().build();
-	String tokenValue = "d4198c3d90103fdfeb3b4bf85e52620f";
-	String retrieveDictionary = "http://challenge.code2040.org/api/prefix";
-	String sendArray = "http://challenge.code2040.org/api/prefix/validate";
-	
-	public void handleAPIRequest() {
-		String strings = retrieveDictionaryFromAPI();
-		JSONArray jArray = convertAPIResponseToJSON(strings);
+	public void handleAPIRequest() throws ClientProtocolException, IOException, JSONException {
+		strings = retrieveDictionaryFromAPI();
+		jArray = convertAPIResponseToJSON(strings);
 		searchList = convertJSONToStringArray(jArray);
 		myList = convertArrayToArrayList(searchList);
 		myList = removeStringsWithPrefix(myList);
@@ -44,114 +43,69 @@ public class Prefix {
 		sendArray(lastJSONArray);
 	}
 	
-	public String retrieveDictionaryFromAPI() {
-		try {
-		HttpPost request = new HttpPost(retrieveDictionary); //Send HTTP POST request
-		request.setEntity((new StringEntity("{\"token\":\"" + tokenValue + "\"}", 
-				ContentType.create("application/json"))));
-		HttpResponse response = client.execute(request); //Retrieves JSON Dictionary
+	private String retrieveDictionaryFromAPI() throws ClientProtocolException, IOException {
+		final String ENDPOINT_FOR_RETRIEVAL = "http://challenge.code2040.org/api/prefix";
+		final HttpPost request = new HttpPost(ENDPOINT_FOR_RETRIEVAL); //Send HTTP POST request
+		request.setEntity((new StringEntity("{\"token\":\"" + TOKEN_VALUE + "\"}", 
+			ContentType.create("application/json"))));
+		HttpResponse response = CLIENT.execute(request); //Retrieves JSON Dictionary
 		HttpEntity entity = response.getEntity();
-		bytes = (EntityUtils.toByteArray(entity)); //ToByteArray in order to turn elements into Strings
+		byte[] bytes = (EntityUtils.toByteArray(entity)); //ToByteArray in order to turn elements into Strings
 		strings = new String(bytes);
-		System.out.println("This is what the API gave me : " + strings); //prints what the API gave
-		return strings;
-		
-		} catch (Exception e) {
-			
-		} finally {
-			
-		}
-		return "";
-		
+		return strings;	
 	}
 	
-	public JSONArray convertAPIResponseToJSON(String apiResponse) {
-		apiResponse = strings;
-		try {
+	private JSONArray convertAPIResponseToJSON(String apiResponse) throws JSONException {
 		JSONObject myObject = new JSONObject(apiResponse); //JSONObject in order to turn into String[]
 		prefixID = myObject.getString("prefix"); //Retrieve prefixID for Substring to String comparison
 		jArray = myObject.getJSONArray("array"); //Plug array into jArray in order to turn into String[]
-		} catch (Exception e) {
-			
-		} finally {
-			
-		}
-		System.out.println("This is my jArray : " + jArray);
 		return jArray;
 	}
 	
-	public String[] convertJSONToStringArray(JSONArray jArray){
-		try {
+	private String[] convertJSONToStringArray(JSONArray jArray) throws JSONException{
 		searchList = new String[jArray.length()]; //turn into String[]
 		for(int i=0;i<jArray.length();i++){
 			searchList[i] = jArray.getString(i);
 		}
-		} catch (Exception e) {
-			
-		} finally {
-			
-		}
-		System.out.println("This is my search List : " + Arrays.toString(searchList));
 		return searchList;
 	}
 	
-	public ArrayList<String> convertArrayToArrayList(String[] array){
+	private ArrayList<String> convertArrayToArrayList(String[] array){
 		myList = new ArrayList<String>(); //turn String[] into ArrayList in order to 
 		//dynamically resize after deletions
 		Collections.addAll(myList, searchList);
-		
-		System.out.println("This is my Array List : " + myList);
 		return myList;
 	}
 	
-	public ArrayList<String> removeStringsWithPrefix(ArrayList<String> myList) {
-		//System.out.println("This is my prefix length : " + prefixID.length());
-		System.out.println("prefixID: " + prefixID);
-		
-
+	private ArrayList<String> removeStringsWithPrefix(ArrayList<String> myList) {
 		Iterator<String> iter = myList.iterator();
-        while(iter.hasNext()){ //Iterate through ArrayList to delete
+        	while(iter.hasNext()){ //Iterate through ArrayList to delete
         	String nextString = iter.next();
-        	
 			if (prefixID.equals(nextString.substring(0,4))) {
 				iter.remove();
 				count++;
 			}
 		}
-        
-        System.out.println("This is my Array List after removing strings : " + myList);
-        return myList;
+        	return myList;
 	}
 	
-	public JSONArray convertArrayListToArray(ArrayList myList) {
+	private JSONArray convertArrayListToArray(ArrayList<String> myList) {
 		int finalLength = searchList.length - count; //Retrieve size of ArrayList after deletions
-        lastList = new String[finalLength]; //Create String[] with ArrayList size after deletions
-        myList.toArray(lastList); //Turn ArrayList into String[]
-        
-        System.out.println("This is my final list : " + Arrays.toString(lastList));
-        lastJSONArray = new JSONArray(Arrays.asList(lastList)); //Turn String[] into JSONArray
-        
-        return lastJSONArray;
+		String[] lastList = new String[finalLength]; //Create String[] with ArrayList size after deletions
+        	myList.toArray(lastList); //Turn ArrayList into String[]
+        	lastJSONArray = new JSONArray(Arrays.asList(lastList)); //Turn String[] into JSONArray
+        	return lastJSONArray;
 	}
 	
-	public void sendArray(JSONArray array) {
-		
-		try {
-			HttpPost request = new HttpPost(sendArray);
-			request.setEntity((new StringEntity("{\"token\":\"" + tokenValue + "\",\"array\":" + lastJSONArray + "}", 
-					ContentType.create("application/json"))));
-			//System.out.println(("{\"token\":\"" + tokenValue + "\",\"array\":" + Arrays.toString(array) + "}"));
-			HttpResponse response = client.execute(request);
+	private void sendArray(JSONArray array) throws ClientProtocolException, IOException {
+		final String ENDPOINT_FOR_VALIDATION = "http://challenge.code2040.org/api/prefix/validate";	
+		final HttpPost request = new HttpPost(ENDPOINT_FOR_VALIDATION);
+		request.setEntity((new StringEntity("{\"token\":\"" + TOKEN_VALUE + "\",\"array\":" + lastJSONArray + "}", 
+			ContentType.create("application/json"))));
+		final HttpResponse response = CLIENT.execute(request);
 			
-			
-			System.out.println("\nSending 'POST' request to URL : " + sendArray);
-			System.out.println("Post parameters : " + request.getEntity());
-			System.out.println("Response Code : " + response.getStatusLine().toString());
-		} catch (Exception e) {
-			
-		} finally {
-			
-		}
+		System.out.println("\nSending 'POST' request to URL : " + ENDPOINT_FOR_VALIDATION);
+		System.out.println("Post parameters : " + request.getEntity());
+		System.out.println("Response Code : " + response.getStatusLine().toString());
 	}
-
 }

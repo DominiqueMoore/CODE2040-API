2d91017c2f69b438fdaeef4fbeb97ea5cd651969
Dominique <dominique_moore@brown.edu>
2016-08-26 12:37:26 -0400

    Update StringReversal.java
    Cleaned up code, final variables, JavaDocs

--- StringReversal.java
+++ StringReversal.java
@@ -1,4 +1,6 @@
+import java.io.IOException;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
@@ -6,68 +8,47 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
-
-public class stringReversal {
+/**
+ * With this exercise, I learned how to use StringBuilder in order to reverse 
+ * a String.
+ */
+public class StringReversal {
 	
-	String stringOne = null;
-	String reversedString = null;
-	String tokenValue = new String("d4198c3d90103fdfeb3b4bf85e52620f");
-	HttpClient client = HttpClientBuilder.create().build();
-
-	public stringReversal() {
-		this.sendString(this.reverseString(this.getString()));
+	private final String TOKEN_VALUE = "d4198c3d90103fdfeb3b4bf85e52620f";
+	private final HttpClient CLIENT = HttpClientBuilder.create().build();
+	private String originalString;
+	private String reversedString;
+	
+	public void handleAPIRequest() throws ClientProtocolException, IOException {
+		getString();
+		reversedString = reverseString(originalString);
+		sendString(reversedString);
 	}
 
-	public String getString() {
-		String getStringEndpoint = new String("http://challenge.code2040.org/api/reverse");
-		
-		try {
-			HttpPost request = new HttpPost(getStringEndpoint);
-			request.setEntity((new StringEntity("{\"token\":\"" + tokenValue + "\"}", 
-					ContentType.create("application/json"))));
-			HttpResponse response = client.execute(request);
-			stringOne = EntityUtils.toString(response.getEntity());
-			
-			reversedString = new StringBuilder(stringOne).reverse().toString();
-			
-			System.out.println("\nSending 'POST' request to URL : " + getStringEndpoint);
-			System.out.println("Post parameters : " + request.getEntity());
-			System.out.println("Response Code : " + response.getStatusLine().toString());
-			System.out.println(stringOne);
-			
-		} catch (Exception e) {
-			
-		} finally {
-			
-		}
-		
-		return stringOne;
+	private String getString() throws ClientProtocolException, IOException {
+		final String ENDPOINT_FOR_RETRIEVAL = "http://challenge.code2040.org/api/reverse";
+		final HttpPost request = new HttpPost(ENDPOINT_FOR_RETRIEVAL);
+		request.setEntity((new StringEntity("{\"token\":\"" + TOKEN_VALUE + "\"}", 
+			ContentType.create("application/json"))));
+		HttpResponse response = CLIENT.execute(request);
+		originalString = EntityUtils.toString(response.getEntity());
+		return originalString;
 	}
 	
-	public String reverseString(String string) {
+	private String reverseString(String string) {
 		reversedString = new StringBuilder(string).reverse().toString();
-		System.out.println(reversedString);
 		return reversedString;
 	}
 	
-	public void sendString(String string) {
-		String sendStringEndpoint = new String("http://challenge.code2040.org/api/reverse/validate");
-	
-		try {
-			HttpPost request = new HttpPost(sendStringEndpoint);
-			request.setEntity((new StringEntity("{\"token\":\"" + tokenValue + "\",\"string\":\"" + reversedString + "\"}", 
-					ContentType.create("application/json"))));
-			HttpResponse response = client.execute(request);
+	private void sendString(String string) throws ClientProtocolException, IOException {
+		final String ENDPOINT_FOR_VALIDATION = "http://challenge.code2040.org/api/reverse/validate";
+		final HttpPost request = new HttpPost(ENDPOINT_FOR_VALIDATION);
+		request.setEntity((new StringEntity("{\"token\":\"" + TOKEN_VALUE + "\",\"string\":\"" + reversedString + "\"}", 
+				ContentType.create("application/json"))));
+		final HttpResponse response = CLIENT.execute(request);
 			
-			System.out.println("\nSending 'POST' request to URL : " + sendStringEndpoint);
-			System.out.println("Post parameters : " + request.getEntity());
-			System.out.println("Response Code : " + response.getStatusLine().toString());
-			System.out.println(reversedString);
-		} catch (Exception e) {
-			
-		} finally {
-			
-		}
-	
+		System.out.println("\nSending 'POST' request to URL : " + ENDPOINT_FOR_VALIDATION);
+		System.out.println("Post parameters : " + request.getEntity());
+		System.out.println("Response Code : " + response.getStatusLine().toString());
 	}
 }
